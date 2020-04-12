e7ffc836027f746d3aded329f7237c1d30ee5e94
Abraham Post <abrahampost@yahoo.com>
2020-04-12 19:32:51 -0400

    Ch. 7 completed added strings

--- memory.h
+++ memory.h
@@ -1,6 +1,11 @@
 #ifndef clox_memory_h
 #define clox_memory_h
 
+#include "object.h"
+
+#define ALLOCATE(type, count) \
+    (type*)reallocate(NULL, 0, sizeof(type) * (count))
+
 #define GROW_CAPACITY(capacity) \
     ((capacity) < 8 ? 8 : (capacity) * 2)
 
@@ -11,6 +16,10 @@
 #define FREE_ARRAY(type, pointer, oldCount) \
     reallocate(pointer, sizeof(type) * (oldCount), 0)
 
+#define FREE(type, pointer) \
+    reallocate(pointer, sizeof(type), 0)
+
 void* reallocate(void* previous, size_t oldSize, size_t newSize);
+void freeObjects();
 
 #endif
